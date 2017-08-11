e9fd4087d1dca0c2aad691cea081cb3929393f1f
v.bilinskyi <bilinskyivitalii@gmail.com>
2017-08-11 13:27:55 +0300

    add validation: https://teamtrack.macc.com.ua/view.php?id=53572

--- wp-content/themes/greencard/assets/javascript/edit-validation.js
+++ wp-content/themes/greencard/assets/javascript/edit-validation.js
@@ -3,27 +3,67 @@
  */
 
 $(document).ready(function() {
+    var lang = $('.current-lang a').attr('lang');
 
     $('#phone, #mobile').inputmask('phone');
     $('#birth_country, #country_resid').select2();
 
-    /*
-    $('input[name="save_account_detailsa"]').click(function(e){
-        var $requiredElements = $('.required').parent().siblings('input');
-        var err = [];
+    var $requiredElements = $('.required').parent().siblings('input');
+    $requiredElements.on('blur input', function(){
+        var $submitBtn = $('input[name="save_account_details"]');
+        var id = $(this).attr('id');
+        var val = $(this).val();
+        var errorMsg;
+        var valid = true;
 
-        console.log($requiredElements);
+        switch(id){
+            case 'account_email':
+                var email_tpl = /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
 
-        $requiredElements.each(function (indx, el) {
-            if(!el.value){
-                err.push(el);
-            }
-        });
+                if(!val){
+                    errorMsg = {
+                        'en-US': 'This field is required.',
+                        'ru-RU': 'Вы пропустили это поле.'
+                    };
+                    valid = false;
+                }else if(!email_tpl.test(val)){
+                    errorMsg = {
+                        'en-US': 'Enter valid email.',
+                        'ru-RU': 'Введите валидный адрес электронной почты.'
+                    };
+                    valid = false;
+                }
+                break;
+            case 'account_first_name':
+            case 'account_last_name':
+                var fio_tpl = /^[A-z-А-я]+$/;
 
-        if(err.length > 0){
-            // add error message after .row-breadcrumbs с ul с классом .woocommerce-error
-            e.preventDefault();
+                if(!val){
+                    errorMsg = {
+                        'en-US': 'This field is required.',
+                        'ru-RU': 'Вы пропустили это поле.'
+                    };
+                    valid = false;
+                }else if(!fio_tpl.test(val)){
+                    errorMsg = {
+                        'en-US': 'The First/Last Name can only contain alphabetic characters.',
+                        'ru-RU': 'Имя/Фамилия должны состоять из букв.'
+                    };
+                    valid = false;
+                }
+        }
+
+        $(this).siblings('.validationError').remove();
+
+        if(valid){
+            $submitBtn.unbind();
+            $(this).removeClass('validation');
+            $submitBtn.attr('disabled', false);
+        }else{
+            var errorElem = '<p style="margin: 0;" class="validationError">' + errorMsg[lang] + '</p>';
+            $(this).addClass('validation');
+            $(this).after(errorElem);
+            $submitBtn.attr('disabled', true);
         }
     });
-    */
 });
